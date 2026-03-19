bccdfc12b7ff2653f9058342818169434fd0783e
Ekaterina Elenets <elenets.katy@gmail.com>
2026-03-19 15:36:10 +0900

    main

--- src/main.js
+++ src/main.js
@@ -7,46 +7,53 @@ import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
-// @todo: подключение
+import {initPagination} from "./components/pagination.js"; // @todo: подключение
 
-
-// Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
 
-/**
- * Сбор и обработка полей из таблицы
- * @returns {Object}
- */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
 
+    // Приводим числа к нужному типу
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
+
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page
     };
 }
 
-/**
- * Перерисовка состояния таблицы при любых изменениях
- * @param {HTMLButtonElement?} action
- */
 function render(action) {
-    let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
+    let state = collectState();
+    let result = [...data];
+
     // @todo: использование
+    result = applyPagination(result, state, action);
 
-
-    sampleTable.render(result)
+    sampleTable.render(result);
 }
 
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
     before: [],
-    after: []
+    after: ['pagination']  // добавляем пагинацию после таблицы
 }, render);
 
 // @todo: инициализация
-
+const applyPagination = initPagination(
+    sampleTable.pagination.elements,
+    (el, page, isCurrent) => {
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+);
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
